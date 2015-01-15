a0eb298dc0f3c62df474f6a17ce8b2f03493aff7
Yutin Liu (劉宇庭) <Yutin.Liu@mediatek.com>
2015-01-16 01:18:32 +0800

    debug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.post('/', function(request, response) {
+app.post('/', function(req, res) {
   res.sendFile('./public/index.html');
 });
 
