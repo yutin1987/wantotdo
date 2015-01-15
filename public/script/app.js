8e389f371fd4c44bf57506020c30da383c2136f0
Yutin Liu (劉宇庭) <Yutin.Liu@mediatek.com>
2015-01-16 03:12:50 +0800

    debug

--- public/script/app.js
+++ public/script/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var $ = window.$;
+
 function onLogin(response) {
   if ('connected' === response.status) {
     $(".friendDialog").fSelector({
@@ -16,15 +17,17 @@ function onLogin(response) {
   }
 }
 
-FB.getLoginStatus(function(response) {
-  // Check login status on load, and if the user is
-  // already logged in, go directly to the welcome message.
-  if ('connected' === response.status) {
-    onLogin(response);
-  } else {
-    // Otherwise, show Login dialog first.
-    FB.login(function(response) {
+$(function(){
+  FB.getLoginStatus(function(response) {
+    // Check login status on load, and if the user is
+    // already logged in, go directly to the welcome message.
+    if ('connected' === response.status) {
       onLogin(response);
-    }, {scope: 'user_friends, user_groups, publish_actions'});
-  }
+    } else {
+      // Otherwise, show Login dialog first.
+      FB.login(function(response) {
+        onLogin(response);
+      }, {scope: 'user_friends, user_groups, publish_actions'});
+    }
+  });
 });
