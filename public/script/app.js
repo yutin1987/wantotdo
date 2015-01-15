646f153fe1f0b28a73682717d2ec93bed236a722
Yutin Liu (劉宇庭) <Yutin.Liu@mediatek.com>
2015-01-16 01:33:26 +0800

    debug

--- public/script/app.js
+++ public/script/app.js
@@ -1,24 +1,5 @@
 'use strict';
 
-window.fbAsyncInit = function() {
-    FB.init({
-      appId: '888195151232497',
-      xfbml: true,
-      version: 'v2.2'
-    });
-
-    // ADD ADDITIONAL FACEBOOK CODE HERE
-  };
-
-(function(d, s, id){
-  var js;
-  var fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = '//connect.facebook.net/en_US/sdk.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 // Place following code after FB.init call.
 
 function onLogin(response) {
