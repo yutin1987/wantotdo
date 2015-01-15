2247ee72951498766aba9b543f556404b0d8d33a
Yutin Liu (劉宇庭) <Yutin.Liu@mediatek.com>
2015-01-16 01:31:15 +0800

    debug

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require('express');
-var fs = require('fs');
 var app = express();
-
-var page = fs.readFileSync('public/index.html');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.post('/', function(req, res) {
-  res.send(page);
+  res.render('index.html');
+});
+
+app.get('/', function(req, res) {
+  res.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
