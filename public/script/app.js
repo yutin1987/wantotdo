35a3d0e6a56a2dc7aa8d627b592991a722b1d1e4
Yutin Liu (劉宇庭) <Yutin.Liu@mediatek.com>
2015-01-16 03:25:48 +0800

    debug

--- public/script/app.js
+++ public/script/app.js
@@ -16,18 +16,3 @@ function onLogin(response) {
     // });
   }
 }
-
-$(function(){
-  FB.getLoginStatus(function(response) {
-    // Check login status on load, and if the user is
-    // already logged in, go directly to the welcome message.
-    if ('connected' === response.status) {
-      onLogin(response);
-    } else {
-      // Otherwise, show Login dialog first.
-      FB.login(function(response) {
-        onLogin(response);
-      }, {scope: 'user_friends, publish_actions'});
-    }
-  });
-});
